e0e457ea19692f43ff1f9b4e96812c65648dfcf4
Yishenting <1099686974@qq.com>
2020-03-18 00:01:38 +0800

    03172359一神e提交

---
+++ src/com/wangyi/www/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.wangyi.www.controller;
+
+public class UserController {
+
+    public void simpleFunction(){
+        System.out.println("这是个建安");
+    }
+}
---
+++ src/com/wangyi/www/entity/User.java
@@ -0,0 +1,9 @@
+package com.wangyi.www.entity;
+
+public class User {
+    private int age;
+    private String name;
+    private String tel;
+
+
+}
